dacbd34d0af00c5b50b2f47a13cd6f470533ae4b
yesicaz <ymzemanate@soy.sena.edu.co>
2024-08-28 23:20:41 -0500

    proyecto juego cartas

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import db from '../backend/src/config/db.js'
+import routes from '../backend/src/routes/index.js'
+dotenv.config()
+const allowedDomains =['http://localhost:5173']
+const corsOptions ={
+    origin: function(origin, callback){
+        if(allowedDomains.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+const app = express()
+const port = process.env.PORT || 8089;
+db()
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, ()=>{
+    console.log(`server working port ${port}`)  
+})
---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectionDB = async ()=>{
+    try{
+        const dbURI ='mongodb+srv://yemaze123yz:HLcHTZv88jU1QpNX@cluster0.s3jk1.mongodb.net/juegoCartas/retryWrites=true&w=majority&appName=Cluster0'
+await mongoose.connect(dbURI)
+console.log("connection established to juegoCartas database")
+    }catch( error){
+        console.log("Error al conectarse a la base de datos")
+    process.exit(1)
+    }
+}
+export default connectionDB
