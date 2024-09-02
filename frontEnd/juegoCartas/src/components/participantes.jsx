f18b388d1332db465c781dd2e9f2e05f4a3a037d
yesicaz <ymzemanate@soy.sena.edu.co>
2024-09-02 06:21:02 -0500

    S

---
+++ frontEnd/juegoCartas/src/components/participantes.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import React from 'react'
+
+export const Participantes = () => {
+  
+    
+  return (
+    <div>Participantes</div>
+  )
+}
--- frontEnd/juegoCartas/src/pages/CrearPartida.jsx
+++ frontEnd/juegoCartas/src/pages/CrearPartida.jsx
@@ -1,8 +1,10 @@
+import { Participantes } from "../components/participantes";
 const CrearPartida = () => {
     return(
        <div>
           <h2>Crear partida</h2>
           <p>Contenido para crear partida.</p>
+<Participantes/>
        </div>);
   };
   
