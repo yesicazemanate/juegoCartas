8401745c611e25ac640f9fa9f01b8e04110ccec7
yesicazemanate <ymzemanate@soy.sena.edu.co>
2024-09-06 08:21:02 -0500

    camios

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const corsOptions ={
       callback(new Error('No permitido por CORS'));  
     }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
 }
 
